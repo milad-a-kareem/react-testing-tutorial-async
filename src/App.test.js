69f8d55aeb177070dd2aeae1e38f063aff627052
Milad <milad.kareem@gmail.com>
2022-02-08 05:29:38 +0300

    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App component test', ()=>{
+  test('renders "click to see the text"', async() => {
+
+    render(<App />);
+  
+
+    const linkElement = screen.getByText('click to see the text')
+      expect(linkElement).toBeInTheDocument();
+  });
+
+  test('clicks and shows the "I am visible I am the text"', async()=>{
+    render(<App />)
+
+    const buttonElement = screen.getByRole('button')
+    fireEvent.click(buttonElement)
+
+    const textElement = await screen.findByText('I am visible I am the text',{}, {timeout: 1000})
+    expect(textElement).toBeInTheDocument()
+
+  })
+
+  test('clicks and will not show "click to see the text"', async()=>{
+    render(<App />)
+
+    const buttonElement = screen.getByRole('button')
+    fireEvent.click(buttonElement)
+
+    await waitForElementToBeRemoved(() => screen.queryByText('click to see the text'))
+
+
+  })
+
+})
+
+
+
